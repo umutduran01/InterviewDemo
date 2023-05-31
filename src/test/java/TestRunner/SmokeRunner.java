15945d2772a24bfa84f47c429585cfd8047a549b
Umut Duran <umutduran.95@hotmail.com>
2023-05-31 17:35:22 +0100

    Jenkins Test

--- src/test/java/TestRunner/SmokeRunner.java
+++ src/test/java/TestRunner/SmokeRunner.java
@@ -5,10 +5,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\Features",
+@CucumberOptions(features = "src/test/resources/Features",
         glue = "StepDefinitions",
-        dryRun = true,
-        tags = "@database",
+        dryRun = false,
+        tags = "@newTest",
         plugin = {"pretty", "html:target/Cucumber.html", "json:target/Cucumber.json", "rerun:target/failed.txt"})
 
 public class SmokeRunner {
